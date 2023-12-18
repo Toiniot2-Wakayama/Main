7e607c29422503e11be7b50337d8ec1a3d12c050
Toiniot2-Wakayama <121283941+Toiniot2-Wakayama@users.noreply.github.com>
2023-12-18 23:10:27 +0900

    Update detail.js

--- main/index/asset/js/detail.js
+++ main/index/asset/js/detail.js
@@ -2,4 +2,11 @@ var gridArticle = document.getElementById("grid-article");
 var articleCount = gridArticle.childElementCount;
 console.log(articleCount);
 
-gridArticle.style.height = String(articleCount * 295)+"px";
+var windowWidth = $(window).width();
+var windowSm = 768;
+if (windowWidth <= windowSm) {
+    gridArticle.style.height = String(articleCount * 260)+"px";
+}
+else{
+    gridArticle.style.height = String(Math.ceil(articleCount / 2) * 500)+"px";
+}
