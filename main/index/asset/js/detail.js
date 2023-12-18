892f64e44e6477a25491ba5f5dc6b77ab4ebe7c8
Toiniot2-Wakayama <121283941+Toiniot2-Wakayama@users.noreply.github.com>
2023-12-18 22:56:59 +0900

    Add files via upload

--- main/index/asset/js/detail.js
+++ main/index/asset/js/detail.js
@@ -1,1 +1,5 @@
+var gridArticle = document.getElementById("grid-article");
+var articleCount = gridArticle.childElementCount;
+console.log(articleCount);
 
+gridArticle.style.height = String(articleCount * 240)+"px";
